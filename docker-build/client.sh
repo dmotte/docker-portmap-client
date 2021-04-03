3186519681ca50854b58aeb660c4d394f1d8015a
Davide Motte <dmotte@dmotte.fakemail>
2021-04-03 02:48:08 +0200

    Added DO_NOT_CHECK_HOST_KEY env var

--- docker-build/client.sh
+++ docker-build/client.sh
@@ -2,11 +2,18 @@
 
 set -ex
 
+# Set any additional options
+ADDITIONAL_OPTIONS=""
+if [ "$DO_NOT_CHECK_HOST_KEY" = "true" ]; then
+    ADDITIONAL_OPTIONS="-o StrictHostKeyChecking=no"
+fi
+
 # Connect and open the tunnel
 ssh \
     -i ~/.ssh/ssh_client_key \
     -o ServerAliveInterval=$KEEPALIVE_INTERVAL \
     -o ExitOnForwardFailure=yes \
+    $ADDITIONAL_OPTIONS \
     $SSH_USERNAME@$SSH_SERVER \
     -p $SSH_PORT \
     -N \
