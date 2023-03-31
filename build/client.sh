ef0333632159501ae94308342a8f0550f98b9b42
dmotte <37443982+dmotte@users.noreply.github.com>
2023-03-31 20:09:28 +0200

    Small code improvements

--- build/client.sh
+++ build/client.sh
@@ -7,12 +7,12 @@ FWDS2=$(echo "$FORWARDINGS" | sed 's/,/ -R /g')
 
 # Connect and open the tunnel
 # shellcheck disable=SC2086
-ssh \
+/usr/bin/ssh \
     -i ~/.ssh/ssh_client_key \
     -o ServerAliveInterval="$KEEPALIVE_INTERVAL" \
     -o ExitOnForwardFailure=yes \
-    $ADDITIONAL_OPTIONS \
-    "$SSH_USERNAME@$SSH_SERVER" \
     -p "$SSH_PORT" \
     -N \
-    -R $FWDS2
+    -R $FWDS2 \
+    $ADDITIONAL_OPTIONS \
+    "$SSH_USERNAME@$SSH_SERVER"
