a7465de9bde75638b0130671f181739c28a4d696
dmotte <37443982+dmotte@users.noreply.github.com>
2023-07-31 15:33:36 +0200

    Removed useless comments and improved client.sh

--- build/client.sh
+++ build/client.sh
@@ -2,17 +2,15 @@
 
 set -ex
 
-# Process the FORWARDINGS list
-FWDS2=$(echo "$FORWARDINGS" | sed 's/,/ -R /g')
+FWDS2="$(echo "$FORWARDINGS" | sed 's/,/ -R /g')"
 
-# Connect and open the tunnel
 # shellcheck disable=SC2046,SC2086
 /usr/bin/ssh \
     -i ~/.ssh/ssh_client_key \
     -o ServerAliveInterval="$KEEPALIVE_INTERVAL" \
     -o ExitOnForwardFailure=yes \
     -p "$SSH_PORT" \
-    $(test $# -eq 0 && echo '-N') \
+    $(test $# -eq 0 && echo '-N' || :) \
     -R $FWDS2 \
     $ADDITIONAL_OPTIONS \
     "$SSH_USERNAME@$SSH_SERVER" \
--- build/startup.sh
+++ build/startup.sh
@@ -2,24 +2,18 @@
 
 set -ex
 
-# Default values for some env vars
 export SSH_USERNAME="${SSH_USERNAME:-portmap}"
 export SSH_PORT="${SSH_PORT:-22}"
 export KEEPALIVE_INTERVAL="${KEEPALIVE_INTERVAL:-30}"
 
-# Copy the ssh_client_key file in the home dir of the portmap user and set the
-# right permissions
 cp /ssh_client_key /home/portmap/.ssh/ssh_client_key
 chown portmap:portmap /home/portmap/.ssh/ssh_client_key
 chmod 600 /home/portmap/.ssh/ssh_client_key
 
-# Copy the known_hosts file (if exists) in the home dir of the portmap user and
-# set the right permissions
 if [ -f /known_hosts ]; then
     cp /known_hosts /home/portmap/.ssh/known_hosts
     chown portmap:portmap /home/portmap/.ssh/known_hosts
     chmod 644 /home/portmap/.ssh/known_hosts
 fi
 
-# Run the client.sh script as the portmap user
 su portmap -c "/home/portmap/client.sh $*"
