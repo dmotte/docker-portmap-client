01575a8f3af3065d791296481cf3cc36438a9b2b
dmotte <37443982+dmotte@users.noreply.github.com>
2023-08-05 02:36:52 +0200

    Simplifications with the install command

--- build/startup.sh
+++ build/startup.sh
@@ -6,14 +6,7 @@ export SSH_USERNAME="${SSH_USERNAME:-portmap}"
 export SSH_PORT="${SSH_PORT:-22}"
 export KEEPALIVE_INTERVAL="${KEEPALIVE_INTERVAL:-30}"
 
-cp /ssh_client_key /home/portmap/.ssh/ssh_client_key
-chown portmap:portmap /home/portmap/.ssh/ssh_client_key
-chmod 600 /home/portmap/.ssh/ssh_client_key
+install -oportmap -gportmap -m600 /ssh_client_key ~portmap/.ssh
+install -oportmap -gportmap -m644 /known_hosts ~portmap/.ssh || :
 
-if [ -f /known_hosts ]; then
-    cp /known_hosts /home/portmap/.ssh/known_hosts
-    chown portmap:portmap /home/portmap/.ssh/known_hosts
-    chmod 644 /home/portmap/.ssh/known_hosts
-fi
-
-su portmap -c "/home/portmap/client.sh $*"
+su portmap -c "~portmap/client.sh $*"
