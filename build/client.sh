fba88deaed0d5a8ade4a2842b69d25ac8e049751
dmotte <37443982+dmotte@users.noreply.github.com>
2024-01-21 19:54:32 +0100

    Shorter Bash equality condition

--- build/client.sh
+++ build/client.sh
@@ -10,7 +10,7 @@ FWDS2=$(echo "$FORWARDINGS" | sed 's/,/ -R /g')
     -o ServerAliveInterval="$KEEPALIVE_INTERVAL" \
     -o ExitOnForwardFailure=yes \
     -p "$SSH_PORT" \
-    $(if [ $# -eq 0 ]; then echo '-N'; fi) \
+    $(if [ $# = 0 ]; then echo '-N'; fi) \
     -R $FWDS2 \
     $ADDITIONAL_OPTIONS \
     "$SSH_USERNAME@$SSH_SERVER" \
