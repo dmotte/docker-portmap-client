15b6b3134acd445d0758071c801f37c29d5990be
dmotte <37443982+dmotte@users.noreply.github.com>
2024-02-10 13:04:41 +0100

    Shortened portmap SSH command

--- build/client.sh
+++ build/client.sh
@@ -7,9 +7,9 @@ FWDS2=$(echo "$FORWARDINGS" | sed 's/,/ -R /g')
 # shellcheck disable=SC2046,SC2086
 /usr/bin/ssh \
     -i ~/.ssh/ssh_client_key \
-    -o ServerAliveInterval="$KEEPALIVE_INTERVAL" \
-    -o ExitOnForwardFailure=yes \
-    -p "$SSH_PORT" \
+    -oServerAliveInterval="$KEEPALIVE_INTERVAL" \
+    -oExitOnForwardFailure=yes \
+    -p"$SSH_PORT" \
     $(if [ $# = 0 ]; then echo '-N'; fi) \
     -R $FWDS2 \
     $ADDITIONAL_OPTIONS \
