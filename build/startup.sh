6a568670a65007e3790b4a773964537b8be8b9ba
dmotte <37443982+dmotte@users.noreply.github.com>
2024-08-25 15:21:16 +0200

    Moved keepalive_interval assignment up

--- build/startup.sh
+++ build/startup.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
+keepalive_interval=${KEEPALIVE_INTERVAL:-30}
+
+################################################################################
+
 install -m600 /ssh_client_key ~portmap/.ssh
 install -m644 /known_hosts ~portmap/.ssh || :
-
-keepalive_interval=${KEEPALIVE_INTERVAL:-30}
 
 # Start the OpenSSH client with "exec" to ensure it receives all the stop
 # signals correctly
