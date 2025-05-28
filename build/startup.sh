743dbe794f090616d04e5f48d0b5ccae82ef9f17
dmotte <37443982+dmotte@users.noreply.github.com>
2025-05-28 19:30:37 +0200

    Improved install cmds

--- build/startup.sh
+++ build/startup.sh
@@ -6,8 +6,8 @@ keepalive_interval=${KEEPALIVE_INTERVAL:-30}
 
 ################################################################################
 
-install -m600 /ssh_client_key ~portmap/.ssh
-install -m644 /known_hosts ~portmap/.ssh || :
+install -m600 -t ~portmap/.ssh /ssh_client_key
+install -m644 -t ~portmap/.ssh /known_hosts || :
 
 # Start the OpenSSH client with "exec" to ensure it receives all the stop
 # signals correctly
